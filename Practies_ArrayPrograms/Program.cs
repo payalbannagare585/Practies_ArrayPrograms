abe6034cfa4ae9b22860f70efa85a79f20f91104
Gaurav <Gaurav@LAPTOP-TREG306B>
2022-09-21 21:38:08 +0530

    Add project files.

---
+++ Practies_ArrayPrograms/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace Practies_ArrayPrograms
+{
+    public class Program
+    {
+        private static bool i;
+
+        static void Main(string[] args)
+        {
+            int[] num = new int[10] { 65, 85, 45, 96, 23, 12, 89, 56, 87, 90 };
+            Console.WriteLine("Orignal array");
+            foreach(int i in num)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("-----------------------------------------------");
+
+            int max = num.Max();
+            Console.WriteLine($"Maximum integer is:{max}");
+
+        }
+    }
+}
